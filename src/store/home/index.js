d1930aa9aa1364b6d3ca7206dc2bd5d7b79b88d4
brocadeyu <1725594062@qq.com>
2022-04-10 15:45:19 +0800

    动态获取三级分类内容

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,12 +1,23 @@
+import { reqCategoryList } from "@/api";
 //home模块的仓库
 const state = {
-
+   categoryList:[]
 };
 const mutations = {
-
+   CategoryList(state,categoryList){
+      state.categoryList=categoryList
+   }
 };
 const actions={
+   async categoryList({commit}){
+      //通过API向服务器发起请求，获取数据
+      let result = await reqCategoryList();
+      console.log(result);
+      if(result.code==200){
+         commit("CategoryList",result.data)
 
+      }
+   }
 };
 const  getters ={
 
